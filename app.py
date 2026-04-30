b85f43b2edd29ef3533c6cfe8b7383cb74a6559d
Joseph Samuel Gwanto <jayr431722@gmail.com>
2026-04-30 22:31:26 +0100

    Added Privacy Policy

--- app.py
+++ app.py
@@ -44,6 +44,11 @@ def about():
     return render_template("about.html")
 
 
+@app.route("/privacy")
+def privacy():
+    return render_template("privacy.html")
+
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
